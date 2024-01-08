1370c2942891e1cea3854da24a53e162eedec023
Omri G <gilharomri@gmail.com>
2024-01-08 17:28:15 +0200

    Develop (#1)
    * Basic v1 router and handlers
    - Readiness handler
    - Error handler
    
    * Added basic RDB backend, auto migration using goose.
    - Update Makefile
    - Added basic authentication
    - Added migration steps
    
    * Added feed route support
    - Create feed
    - Get all feeds
    
    * User can follow feeds now
    - Added new feed follow table
    	- Create feed follow route
    	- Get all feed follows
    	- Delete a feed follow

--- Makefile
+++ Makefile
@@ -1,10 +1,28 @@
 .PHONY clean
 
+dep:
+	go mod tidy
+	go mod vendor
+
 build:
+	dep
 	go build
+linter:
+	gocritic check ./
 
 run:
+	dbup
 	./RSStorage.exe
 
+dbup:
+	goose postgres "user=postgres password=postgres dbname=rssagg sslmode=disable" up
+
+dbupdate:
+	dbup
+
+dbdown:
+	goose postgres "user=postgres password=postgres dbname=rssagg sslmode=disable" down
+
 clean:
+	dbdown
 	rm ./RSStorage.exe
--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/OmriGilhar/RSStorage/internal/database"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
-func main() {
-	fmt.Println("RSS Aggregator Backend")
+type apiConfig struct {
+	DB *database.Queries
+}
 
+func main() {
 	godotenv.Load()
 
 	portString := os.Getenv("PORT")
@@ -18,5 +26,54 @@ func main() {
 		log.Fatal("PORT is not found in the environment")
 	}
 
-	fmt.Println("Port:", portString)
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to DB error:", err)
+	}
+
+	quiries := database.New(conn)
+
+	apiCfg := apiConfig{
+		DB: quiries,
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
+	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
+	v1Router.Post("/feed_follow", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
+	v1Router.Get("/feed_follow", apiCfg.middlewareAuth(apiCfg.handlerGetFeedFollow))
+	v1Router.Delete("/feed_follow/{feedFollowID}", apiCfg.middlewareAuth(apiCfg.handlerDeleteFeedFollow))
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
